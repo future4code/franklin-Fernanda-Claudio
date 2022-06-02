359ad5bf37bbbb63c9cc06ae57c3877ebe13eb95
Fernanda da Silva <nanda.drew01@gmail.com>
2022-06-02 19:10:29 -0300

    fase-finalizacao

--- modulo1/projeto-whatslab/whatslab/src/App.js
+++ modulo1/projeto-whatslab/whatslab/src/App.js
@@ -42,19 +42,49 @@ const ButtonEnter = styled.button`
   padding: 8px 16px;
   cursor: pointer;
 `;
-const StyledMessage = styled.p`
+const BalaoMensagem = styled.div`
   display: flex;
   align-self: flex-start;
+  flex-direction: column;
   color: #03045e;
   list-style: none;
   background-color: white;
-  border-radius: 10px;
   max-width: 60%;
   min-width: 8%;
   margin: 8px;
-  padding: 8px;
+  word-wrap: break-word;
   box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.2);
-  background-color: rgba(142, 227, 239, 0.5);
+  background-color: #f5f5f5;
+  //background-color: rgba(142, 227, 239, 0.5);
+  padding: 0.9em 0.8em;
+  border-radius: 0.5em;
+  font-weight: 400;
+  line-height: 1.3;
+`;
+const BalaoMensagemEu = styled.div`
+  display: flex;
+  align-self: flex-end;
+  flex-direction: column;
+  color: #03045e;
+  list-style: none;
+  background-color: white;
+  max-width: 60%;
+  min-width: 8%;
+  margin: 8px;
+  word-wrap: break-word;
+  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.2);
+  background-color: #ccffcc;
+  padding: 0.9em 0.8em;
+  border-radius: 0.5em;
+  font-weight: 400;
+  line-height: 1.3;
+`;
+
+const ContainerNome = styled.div`
+  color: #8a2be2;
+  font-size: 0.8em;
+  font-weight: 600;
+  margin-bottom: 0.2em;
 `;
 
 function App() {
@@ -66,13 +96,40 @@ function App() {
       texto: 'Olá, comece a enviar suas mensagens agora mesmo!',
     },
   ]);
+
   const ListaDeMensagens = mensagens.map((mensagem) => {
-    return (
-      <StyledMessage>
-        {mensagem.nome}:{mensagem.texto}
-      </StyledMessage>
-    );
+    const removeMensagem = () => {
+      const novaListaMensagem = [...mensagens];
+      const index = novaListaMensagem.findIndex(
+        (Element) => Element === mensagem,
+      );
+      novaListaMensagem.splice(index, 1);
+      // eslint-disable-next-line no-restricted-globals
+      if (confirm('Tem certeza que deseja deletar essa mensagem?') === true) {
+        setMensagens(novaListaMensagem);
+      }
+    };
+    if (
+      mensagem.nome === 'eu' ||
+      mensagem.nome === 'Eu' ||
+      mensagem.nome === 'eU' ||
+      mensagem.nome === 'EU'
+    ) {
+      return (
+        <BalaoMensagemEu onDoubleClick={removeMensagem}>
+          <div>{mensagem.texto}</div>
+        </BalaoMensagemEu>
+      );
+    } else {
+      return (
+        <BalaoMensagem onDoubleClick={removeMensagem}>
+          <ContainerNome>{mensagem.nome}</ContainerNome>
+          <div>{mensagem.texto}</div>
+        </BalaoMensagem>
+      );
+    }
   });
+
   const handleInputNome = (event) => {
     setInputNome(event.target.value);
   };
@@ -88,22 +145,37 @@ function App() {
     setInputNome('');
   };
 
+  const handleKeyPressE = (handleKeyPressE) => {
+    if (handleKeyPressE.key === 'Enter') {
+      const novaMensagem = { nome: inputNome, texto: inputMensagem };
+      const novaListaMensagens = [...mensagens];
+      novaListaMensagens.push(novaMensagem);
+      setMensagens(novaListaMensagens);
+      setInputMensagem('');
+      setInputNome('');
+    }
+  };
+
   return (
     <Container>
       {ListaDeMensagens}
       <Card>
         <UsuarioStyle
+          onKeyPress={handleKeyPressE}
           type="text"
           value={inputNome}
           onChange={handleInputNome}
           placeholder="Usuário"
         />
         <MensagemStyle
+          onKeyPress={handleKeyPressE}
           value={inputMensagem}
           onChange={handleInputMensagem}
           placeholder="Mensagem"
         />
-        <ButtonEnter onClick={inserirMensagem}>Enviar</ButtonEnter>
+        <ButtonEnter onKeyPress={handleKeyPressE} onClick={inserirMensagem}>
+          Enviar
+        </ButtonEnter>
       </Card>
     </Container>
   );
