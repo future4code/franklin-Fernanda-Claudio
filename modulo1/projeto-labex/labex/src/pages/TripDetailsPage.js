18d25a6e40d894e9a0e32e629dd43b5c0c6d5888
Fernanda da Silva <nanda.drew01@gmail.com>
2022-06-28 18:44:16 -0300

    alteracoes

--- modulo1/projeto-labex/labex/src/pages/TripDetailsPage.js
+++ modulo1/projeto-labex/labex/src/pages/TripDetailsPage.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { goToLastPage } from '../routes/coordinator';
 
 const useProtectedPage = () => {
   const navigate = useNavigate();
@@ -17,6 +18,7 @@ const useProtectedPage = () => {
 
 export const TripDetailsPage = () => {
   const [state, setState] = useState([]);
+  const navigate = useNavigate();
 
   useProtectedPage();
   useEffect(() => {
@@ -38,21 +40,23 @@ export const TripDetailsPage = () => {
         console.log('Deu erro: ', error.response);
       });
   }, []);
-  console.log(state);
+
   return (
     <div>
-      <p>{state.name}</p>
-      <p>{state.date}</p>
-      <p>{state.description}</p>
-      <p>{state.durationInDays}</p>
-      <p>{state.planet}</p>
-      <p>{state.name}</p>
-      <h4>Candidatos</h4>
-      <p>{state.candidates[0].name}</p>
-      <p>{state.candidates[0].age}</p>
-      <p>{state.candidates[0].country}</p>
-      <p>{state.candidates[0].profession}</p>
-      <p>{state.candidates[0].applicationText}</p>
+      <button onClick={() => goToLastPage(navigate)}>Voltar</button>
+      <p>
+        {state ? (
+          <p>
+            <p>{state.date}</p>
+            <p>{state.description}</p>
+            <p>{state.durationInDays}</p>
+            <p>{state.planet}</p>
+            <p>{state.name}</p>
+          </p>
+        ) : (
+          <p>false</p>
+        )}
+      </p>
     </div>
   );
 };
